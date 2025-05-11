d78712c5707aff69fb673ef2b1327ece4c3e7348
lavishvs <lv909191@gmail.com>
2025-05-11 21:31:09 +0530

    Updated README.md and config.env

--- server/Middlewares/catchAsyncErrors.js
+++
@@ -1,7 +0,0 @@
-export const catchAsyncErrors = (func) => (req, res, next) => {
-    return (req, res, next) => {
-        Promise.resolve(func(req, res, next)).catch((error) => {
-            next(error);
-        });
-    }
-};
